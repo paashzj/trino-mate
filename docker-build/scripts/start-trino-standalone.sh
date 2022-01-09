9db316d4cd28d81acbc5595df858c2c2eab70bef
lovehzj <1922919664@qq.com>
2022-01-09 19:36:09 +0800

    fix kernel log path

--- docker-build/scripts/start-trino-standalone.sh
+++ docker-build/scripts/start-trino-standalone.sh
@@ -7,4 +7,5 @@ cp $TRINO_HOME/mate/conf/node/standalone/jvm.config $TRINO_HOME/etc/jvm.config
 cp $TRINO_HOME/mate/conf/node/standalone/node.properties $TRINO_HOME/etc/node.properties
 cp $TRINO_HOME/mate/conf/catalog/jmx.properties $TRINO_HOME/etc/catalog/jmx.properties
 cp $TRINO_HOME/mate/conf/catalog/tpch.properties $TRINO_HOME/etc/catalog/jmx.properties
-nohup $TRINO_HOME/bin/launcher run >>/opt/sh/trino/logs/trino.stdout.log 2>>/opt/sh/trino/logs/trino.stderr.log
+nohup $TRINO_HOME/bin/launcher run >>$TRINO_HOME/logs/trino.stdout.log 2>>$TRINO_HOME/logs/trino.stderr.log
+
